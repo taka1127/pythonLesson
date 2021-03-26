c3c18716340d59e8a5694c45375ae3d3e8ffe2c4
taka1127 <taka07251127@gmail.com>
2021-03-26 21:09:58 +0900

    ver01

--- aiapps/carbike/views.py
+++ aiapps/carbike/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# Create your views here.
+from django.template import loader
+from .forms import PhotoForm
+from .models import Photo
+
 def index(request):
-  return HttpResponse("Hellow World!")
+    template = loader.get_template('carbike/index.html')
+    context = {'form': PhotoForm()}
+    return HttpResponse(template.render(context, request))
 
 def predict(request):
-  return HttpResponse("Show predictions")
+    if not request.method == 'POST':
+        return
+        redirect('carbike:index')
+
+    form = PhotoForm(request.POST, request.FILES)
+    if not form.is_valid():
+        raise ValueError('Formが不正です')
+
+    photo = Photo(image=form.cleaned_data['image'])
+    predicted, percentage = photo.predict()
+
+    template = loader.get_template('carbike/result.html')
+
+    context = {
+        'photo_name': photo.image.name,
+        'photo_data': photo.image_src(),
+        'predicted': predicted,
+        'percentage': percentage,
+    }
+
+    return HttpResponse(template.render(context, request))
+
